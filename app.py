92c2a3c92390874c9828cf597b55fa7ea2f7cf9d
srestho0101 <srestho1298@gmail.com>
2025-08-18 12:55:37 +0600

    Adding db for render

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key')
 
 # DB location
-db_file = "/data/data/com.termux/files/home/storage/shared/Skynet/skynet.db"
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_file = os.path.join(basedir, "skynet.db")   # keep DB inside project folder
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_file}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Uploads folder (inside static/Photos)
 UPLOAD_FOLDER = os.path.join(app.root_path, 'static', 'Photos')
